8bbc00036a2ec56ddc006673302d8513dd924b91
StormyGithub <71922194+StormyGithub@users.noreply.github.com>
2020-10-20 18:13:29 +0200

    Fake-Casino-Hack-Source
    Fake-Casino-Hack-Source

---
+++ Fake Casino Hack Source/fake casino hack/main.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace fake_casino_hack
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.metroButton6 = new MetroFramework.Controls.MetroButton();
+            this.metroProgressBar2 = new MetroFramework.Controls.MetroProgressBar();
+            this.metroButton5 = new MetroFramework.Controls.MetroButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.metroProgressBar1 = new MetroFramework.Controls.MetroProgressBar();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Orange;
+            this.label1.Location = new System.Drawing.Point(10, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fake Casino Hack";
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 23;
+            this.metroComboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36"});
+            this.metroComboBox1.Location = new System.Drawing.Point(102, 19);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(149, 29);
+            this.metroComboBox1.Style = MetroFramework.MetroColorStyle.Orange;
+            this.metroComboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Orange;
+            this.label2.Location = new System.Drawing.Point(3, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(243, 5);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(44, 22);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Orange;
+            this.metroButton1.TabIndex = 3;
+            this.metroButton1.Text = "X";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(199, 5);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(44, 22);
+            this.metroButton2.Style = MetroFramework.MetroColorStyle.Orange;
+            this.metroButton2.TabIndex = 4;
+            this.metroButton2.Text = "_";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Highlight = true;
+            this.metroButton3.Location = new System.Drawing.Point(6, 94);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(113, 21);
+            this.metroButton3.Style = MetroFramework.MetroColorStyle.Orange;
+            this.metroButton3.TabIndex = 5;
+            this.metroButton3.Text = "Turn The Hack On";
+            this.metroButton3.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.Highlight = true;
+            this.metroButton4.Location = new System.Drawing.Point(136, 92);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(113, 23);
+            this.metroButton4.Style = MetroFramework.MetroColorStyle.Orange;
+            this.metroButton4.TabIndex = 6;
+            this.metroButton4.Text = "Turn The Hack Off";
+            this.metroButton4.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton4.Click += new System.EventHandler(this.metroButton4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.metroProgressBar1);
+            this.groupBox1.Controls.Add(this.metroButton4);
+            this.groupBox1.Controls.Add(this.metroComboBox1);
+            this.groupBox1.Controls.Add(this.metroButton3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(15, 76);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(257, 136);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.metroButton6);
+            this.groupBox2.Controls.Add(this.metroProgressBar2);
+            this.groupBox2.Controls.Add(this.metroButton5);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox2.Location = new System.Drawing.Point(15, 218);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(257, 107);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "injection";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // metroButton6
+            // 
+            this.metroButton6.Highlight = true;
+            this.metroButton6.Location = new System.Drawing.Point(127, 71);
+            this.metroButton6.Name = "metroButton6";
+            this.metroButton6.Size = new System.Drawing.Size(103, 23);
+            this.metroButton6.Style = MetroFramework.MetroColorStyle.Orange;
+            this.metroButton6.TabIndex = 14;
+            this.metroButton6.Text = "Re-Inject";
+            this.metroButton6.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton6.Click += new System.EventHandler(this.metroButton6_Click);
+            // 
+            // metroProgressBar2
+            // 
+            this.metroProgressBar2.Location = new System.Drawing.Point(12, 36);
+            this.metroProgressBar2.Name = "metroProgressBar2";
+            this.metroProgressBar2.Size = new System.Drawing.Size(198, 23);
+            this.metroProgressBar2.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroProgressBar2.TabIndex = 3;
+            this.metroProgressBar2.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // metroButton5
+            // 
+            this.metroButton5.Highlight = true;
+            this.metroButton5.Location = new System.Drawing.Point(12, 71);
+            this.metroButton5.Name = "metroButton5";
+            this.metroButton5.Size = new System.Drawing.Size(103, 23);
+            this.metroButton5.Style = MetroFramework.MetroColorStyle.Orange;
+            this.metroButton5.TabIndex = 7;
+            this.metroButton5.Text = "Inject Growtopia";
+            this.metroButton5.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.metroButton5.Click += new System.EventHandler(this.metroButton5_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Orange;
+            this.label3.Location = new System.Drawing.Point(14, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Injection";
+            // 
+            // metroProgressBar1
+            // 
+            this.metroProgressBar1.Location = new System.Drawing.Point(12, 54);
+            this.metroProgressBar1.Name = "metroProgressBar1";
+            this.metroProgressBar1.Size = new System.Drawing.Size(198, 23);
+            this.metroProgressBar1.Style = MetroFramework.MetroColorStyle.Green;
+            this.metroProgressBar1.TabIndex = 15;
+            this.metroProgressBar1.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 333);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "main";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Orange;
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.main_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroButton metroButton4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private MetroFramework.Controls.MetroButton metroButton6;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar2;
+        private MetroFramework.Controls.MetroButton metroButton5;
+        private System.Windows.Forms.Label label3;
+        private MetroFramework.Controls.MetroProgressBar metroProgressBar1;
+    }
+}
+
---
+++ Fake Casino Hack Source/fake casino hack/main.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//this fake casino hack is made by Storm#4684
+//if you have any bugs dm Storm#4684!
+namespace fake_casino_hack
+{
+    public partial class main : MetroFramework.Forms.MetroForm
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer1.Interval = 350; //control text speed flashig
+            timer2.Start();
+            timer2.Interval = 350; //control text speed flashig
+            timer3.Interval = 350; //control text speed flashig
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            int A = r.Next(0, 255);
+            int R = r.Next(0, 255);
+            int G = r.Next(0, 255);
+            int B = r.Next(0, 255);
+
+            label1.ForeColor = Color.FromArgb(A, R, G, B);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            int A = r.Next(0, 255);
+            int R = r.Next(0, 255);
+            int G = r.Next(0, 255);
+            int B = r.Next(0, 255);
+
+            label2.ForeColor = Color.FromArgb(A, R, G, B);
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            int A = r.Next(0, 255);
+            int R = r.Next(0, 255);
+            int G = r.Next(0, 255);
+            int B = r.Next(0, 255);
+
+            label3.ForeColor = Color.FromArgb(A, R, G, B);
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+            return;
+        }
+
+        private void metroButton6_Click(object sender, EventArgs e)
+        {
+            (metroProgressBar2).Value = (0);
+        }
+
+        private void metroButton5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Loading...");
+            (metroProgressBar2).Value = (100);
+            MessageBox.Show("The Growtopia.exe Has Been injected!");
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Loading...");
+            (metroProgressBar1).Value = (100);
+            MessageBox.Show("The Casino Hack Has Started!");
+        }
+
+        private void metroButton4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Loading...");
+            (metroProgressBar1).Value = (0);
+            MessageBox.Show("The Casino Hack Has Stopped!");
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
